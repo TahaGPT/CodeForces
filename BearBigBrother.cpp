3e91573c79b9b4c00e54b4ceee12834d5ed5d671
Taha Ahmed <150026779+TahaGPT@users.noreply.github.com>
2025-04-03 23:34:27 +0500

    n increment

--- BearBigBrother.cpp
+++ BearBigBrother.cpp
@@ -1,17 +1,15 @@
-// https: // codeforces.com/problemset/problem/791/A
-
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
     int a, b, n = 0;
     cin >> a >> b;
-    while (a <= b)
+    while(a <= b)
     {
+        a *= 3;
+        b *= 2;
         n++;
-        a = a * 3 * n;
-        b = b * 2 * n;
     }
-    cout << n << endl;
+    cout << n;
+    return 0;
 }
