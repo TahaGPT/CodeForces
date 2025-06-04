d42f1dd3f4f11c1913c7c5dbf5ab544174f60858
TahaGPT <tahaaahmed12115@gmail.com>
2025-06-04 16:53:04 +0500

    Similar Colors

---
+++ StonesTable.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/266/A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, c = 0;
+    cin >> n;
+    string num;
+    cin >> num;
+    for(int i = 1 ; i < n ;i++)
+    {
+        if(num[i - 1] == num[i])
+        {
+            c++;
+            // i--;
+        }
+    }
+    cout << c << endl;
+}
